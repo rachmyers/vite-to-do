be8a99ff7c412277e7618606bcca0cd62193b0a1
Rachel Dempsey <rachelmyers52@gmail.com>
2025-09-08 13:54:09 -0500

    Fixed the "Return array of tasks" test and added the "Should throw error if response not OK" test

--- src/actions/__tests__/actions.test.js
+++ src/actions/__tests__/actions.test.js
@@ -1,12 +1,37 @@
 import { getAllTasks } from "../actions";
-import { vi, describe, it } from "vitest";
+import { vi, describe, it, expect } from "vitest";
 
+//run using npm test
 global.fetch = vi.fn();
 describe("getAllTasks", () => {
-  it("returnArrayOfTasks", async () => {
-    const mockResonse = [{ id: 1, name: "test", isComplete: false }];
-    fetch.mockResolvedValue(mockResonse);
+  it("Return array of tasks", async () => {
+    //arrange
+    const mockResponseData = [{ id: 1, name: "test", isComplete: false }];
+    const mockResponse = {
+      ok: true,
+      status: 200,
+      json: async () => mockResponseData,
+    };
+    fetch.mockResolvedValue(mockResponse);
+    //act
     const result = await getAllTasks();
-    console.log(result);
+    //assert
+    expect(result).toBe(mockResponseData);
+  });
+
+  it("Should throw error if response not OK", async () => {
+    //arrange
+    const mockResponse = {
+      ok: false,
+      status: 500,
+    };
+    fetch.mockResolvedValue(mockResponse);
+    //act
+    //const result = await getAllTasks();
+    //assert
+    //console.log(result);
+    await expect(getAllTasks()).rejects.toThrowError();
   });
 });
+
+//Write test cases for other actions
