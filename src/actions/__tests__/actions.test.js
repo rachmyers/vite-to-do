da58e4443ba7936d7529da120c82afe6b1e947a3
Rachel Dempsey <rachelmyers52@gmail.com>
2025-09-22 11:14:31 -0500

    Added unit test for createTask

--- src/actions/__tests__/actions.test.js
+++ src/actions/__tests__/actions.test.js
@@ -1,7 +1,8 @@
-import { getAllTasks } from "../actions";
+import { getAllTasks, createTask } from "../actions";
 import { vi, describe, it, expect } from "vitest";
 
 //run using npm test
+//getAllTasks test
 global.fetch = vi.fn();
 describe("getAllTasks", () => {
   it("Return array of tasks", async () => {
@@ -34,4 +35,36 @@ describe("getAllTasks", () => {
   });
 });
 
-//Write test cases for other actions
+//createTask test
+global.fetch = vi.fn();
+describe("createTask", () => {
+  it("Create a task", async () => {
+    //arrange
+    const mockNewTask = [{ id: 1, name: "test", isComplete: false }];
+    const mockResponse = {
+      ok: true,
+      status: 200,
+      json: async () => mockNewTask,
+    };
+    fetch.mockResolvedValue(mockResponse);
+    //act
+    const result = await createTask(mockNewTask);
+    //assert
+    expect(result).toBe(mockNewTask);
+  });
+
+  it("Should throw error if response not OK", async () => {
+    //arrange
+    const mockNewTask = [{ id: 1, name: "test", isComplete: false }];
+    const mockResponse = {
+      ok: false,
+      status: 500,
+    };
+    fetch.mockResolvedValue(mockResponse);
+    //act
+    //const result = await getAllTasks();
+    //assert
+    //console.log(result);
+    await expect(createTask(mockNewTask)).rejects.toThrowError();
+  });
+});
