f4ce082bfe339fcb479d71a4f8a3cb34a9a8af8c
Rachel Dempsey <rachelmyers52@gmail.com>
2025-10-20 12:01:52 -0500

    Continued work on react testing library

--- src/components/TodoForm.test.jsx
+++ src/components/TodoForm.test.jsx
@@ -1,10 +1,18 @@
 import TodoForm from "./TodoForm";
 import { render, screen } from "@testing-library/react";
 import { vi, describe, it, expect } from "vitest";
+import userEvent from "@testing-library/user-event";
 
 describe("TodoForm", () => {
   it("Should render submit form button", () => {
     render(<TodoForm />);
     expect(screen.getByText("create task")).toBeInTheDocument();
+  });
+  it("Should add and show a new task", () => {
+    render(<TodoForm />);
+    const textInput = screen.getAllByPlaceholderText("type here");
+    userEvent.type(textInput, "new task");
+    screen.getByText("create task").click();
+    //expect(screen.getByText("create task")).toBeInTheDocument();
   });
 });
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-})
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./setup.js",
+  },
+});
